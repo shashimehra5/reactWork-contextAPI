53278204b5df2ede7e43614891f90594215eb9ac
shashi mehra <shashi.mehra@monsterindia.com>
2019-04-07 23:14:16 +0530

    Updates

---
+++ precache-manifest.238cede2c4245fd15db02bc2d3e89f70.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab0edda0e2b39443607be26615532844",
+    "url": "/reactWork-contextAPI/static/media/result5.ab0edda0.png"
+  },
+  {
+    "revision": "adf221d2113e73abbbf6",
+    "url": "/reactWork-contextAPI/static/css/main.c2a87348.chunk.css"
+  },
+  {
+    "revision": "be15ee4ff9b49a15ccaf",
+    "url": "/reactWork-contextAPI/static/js/runtime~main.5709668f.js"
+  },
+  {
+    "revision": "1de4e3227c793f1b2a3d",
+    "url": "/reactWork-contextAPI/static/js/2.683b2ea5.chunk.js"
+  },
+  {
+    "revision": "c98c8ec00e40c6784378605e07476d54",
+    "url": "/reactWork-contextAPI/static/media/bannerbig.c98c8ec0.jpg"
+  },
+  {
+    "revision": "2be25ffec0cfb264e8652bad1a1a2857",
+    "url": "/reactWork-contextAPI/static/media/result1.2be25ffe.png"
+  },
+  {
+    "revision": "404ede052ab8da8950e86fa4fbb7ee46",
+    "url": "/reactWork-contextAPI/static/media/result2.404ede05.png"
+  },
+  {
+    "revision": "f219a65e31ef0fdb90a06beb84567160",
+    "url": "/reactWork-contextAPI/static/media/result3.f219a65e.png"
+  },
+  {
+    "revision": "ee9a3642fe3975ecad5aba75e6a710b1",
+    "url": "/reactWork-contextAPI/static/media/result4.ee9a3642.png"
+  },
+  {
+    "revision": "adf221d2113e73abbbf6",
+    "url": "/reactWork-contextAPI/static/js/main.c4ca60d9.chunk.js"
+  },
+  {
+    "revision": "178b2c3d58a1aeaa4fb2e3eea28cf9af",
+    "url": "/reactWork-contextAPI/static/media/result6.178b2c3d.png"
+  },
+  {
+    "revision": "ec6dacb8e0f3f24201b690db75af2be6",
+    "url": "/reactWork-contextAPI/static/media/result7.ec6dacb8.png"
+  },
+  {
+    "revision": "bbe1d113f1fff55af8481d61736f00bf",
+    "url": "/reactWork-contextAPI/static/media/result8.bbe1d113.png"
+  },
+  {
+    "revision": "c94bd4b8e1d7da659c2d070003b8a1fa",
+    "url": "/reactWork-contextAPI/static/media/result9.c94bd4b8.png"
+  },
+  {
+    "revision": "ce5943d191e063ef06fb2fc725309330",
+    "url": "/reactWork-contextAPI/static/media/result10.ce5943d1.png"
+  },
+  {
+    "revision": "1079264466109914bfe71ed3687dbbaf",
+    "url": "/reactWork-contextAPI/static/media/transavia.10792644.png"
+  },
+  {
+    "revision": "ec3791a25ae787adca4a9c67234c0483",
+    "url": "/reactWork-contextAPI/static/media/panasonic.ec3791a2.png"
+  },
+  {
+    "revision": "289368f70d0852c3cef616d75bb0b7ae",
+    "url": "/reactWork-contextAPI/index.html"
+  }
+];
